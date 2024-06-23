e36dfd30495834212c5e9297441f407a40f6421a
genwo123 <76772410+genwo123@users.noreply.github.com>
2024-06-24 00:18:54 +0900

    개인 프로젝트 첫 업로드

---
+++ Source/BridgeRun.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class BridgeRunTarget : TargetRules
+{
+	public BridgeRunTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("BridgeRun");
+	}
+}
