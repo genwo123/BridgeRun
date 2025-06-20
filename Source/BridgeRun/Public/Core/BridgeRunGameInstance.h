6b558d6b57f80b3f05920e7a4dc0a77ca5b573e7
DESKTOP-439C496\Minseo <rjsdn9008@gmail.com>
2025-06-21 00:08:35 +0900

    Steam 친구창까지 작업 완료 Photon 서버는 문제점이 많아서 제외함

--- Source/BridgeRun/Public/Core/BridgeRunGameInstance.h
+++ Source/BridgeRun/Public/Core/BridgeRunGameInstance.h
@@ -1,22 +1,7 @@
 // Copyright BridgeRun Game, Inc. All Rights Reserved.
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
-
-//  Photon 헤더 추가
-#ifdef _EG_WINDOWS_PLATFORM
-#include "AllowWindowsPlatformTypes.h"
-#endif
-#pragma warning (disable: 4263)
-#pragma warning (disable: 4264)
-#include "LoadBalancing-cpp/inc/Client.h"
-#pragma warning (default: 4263)
-#pragma warning (default: 4264)
-#ifdef _EG_WINDOWS_PLATFORM
-#include "HideWindowsPlatformTypes.h"
-#endif
-
 #include "BridgeRunGameInstance.generated.h"
 
 /**
@@ -56,7 +41,7 @@ struct BRIDGERUN_API FPlayerTeamInfo
 };
 
 /**
- * 게임 인스턴스 클래스 - 로비 + 팀 관리 + Photon 통합
+ * 게임 인스턴스 클래스 - 로비 + 팀 관리 통합
  */
 UCLASS(BlueprintType, Blueprintable, Config = Game)
 class BRIDGERUN_API UBridgeRunGameInstance : public UGameInstance
@@ -66,51 +51,6 @@ class BRIDGERUN_API UBridgeRunGameInstance : public UGameInstance
 public:
     UBridgeRunGameInstance();
     virtual void Init() override;
-    virtual void Shutdown() override;
-
-    // =========================
-    //  Photon 네트워크 관리
-    // =========================
-
-    /** Photon 서버에 연결 */
-    UFUNCTION(BlueprintCallable, Category = "Photon")
-    void ConnectToPhoton();
-
-    /** Photon 서버 연결 해제 */
-    UFUNCTION(BlueprintCallable, Category = "Photon")
-    void DisconnectFromPhoton();
-
-    /** 방 생성 또는 참가 */
-    UFUNCTION(BlueprintCallable, Category = "Photon")
-    void CreateOrJoinRoom(const FString& RoomName = "BridgeRun_Main");
-
-    /** 방 나가기 */
-    UFUNCTION(BlueprintCallable, Category = "Photon")
-    void LeaveRoom();
-
-    /** Photon 연결 상태 확인 */
-    UFUNCTION(BlueprintPure, Category = "Photon")
-    bool IsConnectedToPhoton() const;
-
-    /** 방 참가 상태 확인 */
-    UFUNCTION(BlueprintPure, Category = "Photon")
-    bool IsInRoom() const;
-
-    /** 현재 방 이름 가져오기 */
-    UFUNCTION(BlueprintPure, Category = "Photon")
-    FString GetCurrentRoomName() const;
-
-    /** 현재 방 플레이어 수 */
-    UFUNCTION(BlueprintPure, Category = "Photon")
-    int32 GetRoomPlayerCount() const;
-
-    /** Photon App ID 설정 */
-    UFUNCTION(BlueprintCallable, Category = "Photon")
-    void SetPhotonAppID(const FString& NewAppID);
-
-    /** Photon 연결 상태 메시지 */
-    UFUNCTION(BlueprintPure, Category = "Photon")
-    FString GetPhotonStatusMessage() const;
 
     // =========================
     // 로비 시스템 호환성 (기존 LobbyGameInstance 대체)
@@ -196,12 +136,18 @@ public:
     UFUNCTION(BlueprintCallable, Category = "Teams")
     void PrintPlayersTeamInfo() const;
 
+
+
+
     // =========================
     // 팀 균형 관리 함수
     // =========================
 
     UPROPERTY(BlueprintReadWrite, Category = "Teams")
     TArray<int32> StoredTeamCounts;
+
+
+
 
     UFUNCTION(BlueprintPure, Category = "Teams")
     bool AreTeamsBalanced() const;
@@ -221,6 +167,7 @@ public:
     UPROPERTY(BlueprintReadWrite, Category = "UI Navigation")
     bool bSkipLoginScreen = false;
 
+
     UPROPERTY(BlueprintReadWrite, Category = "UI Navigation")
     bool bHasPlayerName = false;
 
@@ -228,41 +175,6 @@ public:
     bool bReturningFromGame = false;
 
 protected:
-    // =========================
-    //  Photon 관련 변수들
-    // =========================
-
-    /** Photon 클라이언트 인스턴스 */
-    ExitGames::LoadBalancing::Client* PhotonClient;
-
-    /** Photon 연결 상태 */
-    UPROPERTY(BlueprintReadOnly, Category = "Photon")
-    bool bIsPhotonConnected = false;
-
-    /** 방 참가 상태 */
-    UPROPERTY(BlueprintReadOnly, Category = "Photon")
-    bool bIsInRoom = false;
-
-    /** Photon App ID */
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Photon", meta = (DisplayName = "Photon App ID"))
-    FString PhotonAppID = "your-photon-app-id-here";
-
-    /** 현재 방 이름 */
-    UPROPERTY(BlueprintReadOnly, Category = "Photon")
-    FString CurrentRoomName;
-
-    /** Photon 연결 상태 메시지 */
-    UPROPERTY(BlueprintReadOnly, Category = "Photon")
-    FString PhotonStatusMessage = "Disconnected";
-
-    /** 자동 연결 활성화 (배포용) */
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Photon")
-    bool bAutoConnectOnStartup = true;
-
-    // =========================
-    // 기존 팀 시스템 변수들
-    // =========================
-
     // 팀 점수 배열
     UPROPERTY(BlueprintReadWrite, Category = "Teams")
     TArray<int32> TeamScores;
@@ -292,27 +204,4 @@ protected:
 private:
     // 싱글톤 패턴 지원
     static TWeakObjectPtr<UBridgeRunGameInstance> Instance;
-
-    // =========================
-    //  Photon 내부 함수들
-    // =========================
-
-    /** Photon 클라이언트 초기화 */
-    void InitializePhotonClient();
-
-    /** Photon 클라이언트 정리 */
-    void CleanupPhotonClient();
-
-    /** Photon 이벤트 처리 (Tick에서 호출) */
-    void UpdatePhotonClient();
-
-    /** Photon 콜백 함수들 */
-    void OnPhotonConnected();
-    void OnPhotonDisconnected();
-    void OnPhotonJoinedRoom();
-    void OnPhotonLeftRoom();
-    void OnPhotonError(const FString& ErrorMessage);
-
-    /** 타이머 핸들 */
-    FTimerHandle PhotonUpdateTimerHandle;
 };
